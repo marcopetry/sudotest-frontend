55bedc72650a70bc5d0e150b6ad954a3164f1598
marcopetry <marcomattospetry@gmail.com>
2019-09-30 16:23:37 -0300

    Telas prontas

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,8 @@ import CadastroQuestoes from './components/CadastroQuestoes/CadastroQuestoes';
 export default function Routes() {
     return (
         <BrowserRouter>
-            {/* <Route path="/" exact component={Login}/> */}
-            <Route path="/" exact component={CadastroQuestoes}/>            
+            <Route path="/" exact component={Login}/>
+            <Route path="/cadastro_questoes" exact component={CadastroQuestoes}/>            
             <Route path="/questoes" exact component={Questoes}/> 
             <Route path="/autocadastro" exact component={Autocadastro}/>
         </BrowserRouter>
