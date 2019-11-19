836c68d5fdbb42413961beb754505dc04718e0c7
marcopetry <marcomattospetry@gmail.com>
2019-11-19 17:27:30 -0300

    Ajustes no componente ListaProvas, ajuste de width nos componentes Dashboards e excluído o componente Header que não estava sendo utilizado

--- src/components/ListaProvas/ListaProvas.js
+++ src/components/ListaProvas/ListaProvas.js
@@ -5,20 +5,45 @@ export default function ListaProvas(){
 
     return(
         <div className="container-lista-provas">
-            <table>
+            <table cellpadding="0" cellspacing="0">
                 <thead className="cabecalho-tabela">
                     <tr>
+                        <th className="border-left"></th>
                         <th>Data prova</th>
-                        <th>Porcentagem de aprovação</th>
+                        <th>Quantidade de vagas</th>
                         <th>Quantidade de aprovados</th>
+                        <th>Média geral</th>
+                        <th className="border-right">Status</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
+                        <td className="border-left"><input type="checkbox" /></td>
                         <td>20/12/2020</td>
                         <td>60</td>
                         <td>20</td>
-                        <td className="container-btn"><button className="btn-ver-mais">Ver mais</button></td>
+                        <td>60</td>
+                        <td className="border-right">Aberto</td>
+                    </tr>
+                    <tr>
+                        <td className="border-left"><input type="checkbox" /></td>
+                        <td>20/12/2020</td>
+                        <td>60</td>
+                        <td>20</td>
+                        <td>60</td>
+                        <td className="border-right">Aberto</td>
+                    </tr>
+                    <tr>
+                        <td className="border-left"><input type="checkbox" /></td>
+                        <td>20/12/2020</td>
+                        <td>60</td>
+                        <td>20</td>
+                        <td>60</td>
+                        <td className="border-right">Aberto</td>
+                    </tr>
+                    <tr className="btn-container">
+                        <td className="border-left col-btn" colSpan="3"><button className="btn-table">Marcar todos</button></td>
+                        <td className="border-right col-btn" colSpan="3"><button className="btn-table">Ver mais</button></td>
                     </tr>
                 </tbody>
             </table>
