83edfa4f88c494c9c56497837a73ad90afdffafe
Gabriel <gabrielopes31@hotmail.com>
2019-11-26 18:54:07 -0300

    Checkout

--- src/controlers/ControllerProva.js
+++ src/controlers/ControllerProva.js
@@ -2,19 +2,29 @@ import React, { useState, useEffect } from 'react';
 import Prova from '../components/Prova/Prova';
 import TelaConfirmacao from '../components/TelaConfirmacao/TelaConfirmacao';
 import TelaEspera from '../components/TelaEspera/TelaEspera';
-
-const prova = localStorage.getItem('prova');
-localStorage.removeItem('prova');
+import api from '../services/api';
 
 export default function ControllerProva(props) {
     const [emExecucao, setExecucao] = useState(true),
         [acao, setAcao] = useState(props.acaoEscolhida), 
-        [questoesProva, setQuestoes] = useState();
+        [questoesProva, setQuestoes] = useState(),
+        [prova, setProva] = useState(JSON.parse(localStorage.getItem('prova')));
 
     useEffect(() => {
         setAcao(props.acaoEscolhida);
     }, [props.acaoEscolhida]);
 
+    async function buscarQuestoes(e) {
+        //e.preventDefault(e);
+
+        const response = await api.get('/buscaProvasQuestoes', {
+            params: {
+                idProva: prova.id
+            }
+        })
+        console.log(response);
+    }
+    buscarQuestoes();
     const encerrarSessao = () => {
         localStorage.setItem('Usuario', 'user');
         props.history.push('/home');
