74ad93e1aceb86eb91730fad0d8007c2ccce6147
marcopetry <marcomattospetry@gmail.com>
2019-11-28 16:16:02 -0300

    Informações para sincronizar dashboard com a prova ajustadas

--- src/controlers/ControllerInicial.js
+++ src/controlers/ControllerInicial.js
@@ -7,18 +7,65 @@ import { acoesAluno, acoesProva } from './ControllerAluno';
 import ControllerProva from './ControllerProva';
 import Login from '../components/Login/Login';
 import TelaConfirmacao from '../components/TelaConfirmacao/TelaConfirmacao';
+import api from '../services/api';
+import { preencherListaComRespostasVazias } from '../helpers/MonitorQuestoesProva';
+import { buscarQuestoes } from './ControllerProva';
+
+let listaRespostasVazias = [];
+
+/* async function buscarQuestoesProva() {
+    listaRespostasVazias = await buscarQuestoes();
+} */
 
 export default function ControlerInicial({ history }) {
     //verificar se é usuário ou adm e passar informações pra dash
     const [acao, setAcao] = useState('home'),
-        [tipoUsuario, setUsuario] = useState(localStorage.getItem('Usuario'));
+        [tipoUsuario, setUsuario] = useState(localStorage.getItem('Usuario')), 
+        [questoes, setQuestoes] = useState(''), 
+        [execucao, setExecucao] = useState('');
 
     let acoes;
+    
+
+    /* async function buscarQuestoes(e) {
+        const prova = JSON.parse(localStorage.getItem('prova'));
+        console.log('prova', prova);
+        const response = await api.get('/buscaProvasQuestoes', {
+            params: {
+                idProva: prova.id
+            }
+        })
+        if (response) {
+            const idAluno = localStorage.getItem('idUsuario');
+            response.data.map( questao => {
+                preencherListaComRespostasVazias(idAluno, prova.id, questao.id, listaRespostasVazias)
+            });
+            console.log('lista no inicio', listaRespostasVazias);
+        }else{
+            //validar quando questões não são retornadas
+            setExecucao('feedback');
+            console.log(response, ' deu problema');
+        }
+    } */
+
+    
     if (tipoUsuario === 'adm') {
         acoes = acoesADM;
     } else if (tipoUsuario === 'user') {
         acoes = acoesAluno;
     } else if (tipoUsuario === 'user-prova') {
+        async function buscarQuestoesProva() {
+            listaRespostasVazias = [];
+            listaRespostasVazias = await buscarQuestoes();
+            setAcao('resposta');
+            console.log(listaRespostasVazias);
+        }
+        console.log('lista', listaRespostasVazias)
+        if(listaRespostasVazias.length === 0){
+            buscarQuestoesProva();
+        }
+        
+        console.log(acao);
         acoes = acoesProva;
     } else {
         history.push('/');
@@ -26,7 +73,7 @@ export default function ControlerInicial({ history }) {
     }
 
     const trocarAcao = (e) => setAcao(e);
-
+    
     return (
         <>
             <Dashboards mudarAtividade={trocarAcao} acoesUsuario={acoes} history={history} />
--- src/controlers/ControllerProva.js
+++ src/controlers/ControllerProva.js
@@ -6,42 +6,46 @@ import api from '../services/api';
 import { preencherListaComRespostasVazias } from '../helpers/MonitorQuestoesProva';
 import Feedback from '../components/Feedback/Feedback';
 
+//Uma lista com todas as respostas vazias do usuário para gerenciar no front-end
 let listaRespostasVazias = [];
+//Uma lista com todas as questões para mandar pro componente prova
+let questoesProva = [];
+
+export async function buscarQuestoes() {
+    const prova = JSON.parse(localStorage.getItem('prova'));
+
+    const response = await api.get('/buscaProvasQuestoes', {
+        params: {
+            idProva: prova.id
+        }
+    })
+    if (response) {
+        const idAluno = localStorage.getItem('idUsuario');
+        questoesProva = response.data;
+        listaRespostasVazias = [];
+        response.data.map(questao => {
+            preencherListaComRespostasVazias(idAluno, prova.id, questao.id, listaRespostasVazias)
+        });
+    } else {
+        //validar quando questões não são retornadas
+        console.log(response, ' deu problema');
+    }
+    return listaRespostasVazias;
+}
 
 export default function ControllerProva(props) {
     const [emExecucao, setExecucao] = useState(''),
-        [prova, setProva] = useState(JSON.parse(localStorage.getItem('prova'))),
         [acao, setAcao] = useState(props.acaoEscolhida),
-        [espera, setEspera] = useState(true),
-        [questoesProva, setQuestoes] = useState('');
+        [espera, setEspera] = useState(false);
 
+    console.log('questoes', questoesProva);
+    console.log('respostas vazias', listaRespostasVazias);
+
+    //pra mandar o id da prova como props para gerenciar o front de respostas    
+    const prova = useState(JSON.parse(localStorage.getItem('prova')));
     useEffect(() => {
         setAcao(props.acaoEscolhida);
     }, [props.acaoEscolhida]);
-
-
-    async function buscarQuestoes(e) {
-        const response = await api.get('/buscaProvasQuestoes', {
-            params: {
-                idProva: prova.id
-            }
-        })
-        setQuestoes(response.data);
-        if (response) {
-            const idAluno = localStorage.getItem('idUsuario');
-            response.data.map( questao => {
-                preencherListaComRespostasVazias(idAluno, prova.id, questao.id, listaRespostasVazias)
-            });
-        }else{
-            //validar quando questões não são retornadas
-            setExecucao('feedback');
-            console.log(response, ' deu problema');
-        }
-        
-        setEspera(false);
-    }
-
-    if (questoesProva === '') buscarQuestoes();
 
     const encerrarSessao = () => {
         localStorage.setItem('Usuario', 'user');
@@ -58,15 +62,18 @@ export default function ControllerProva(props) {
             funcaoCancelar={cancelar}
             mensagem={mensagemSaida} />
 
-    if (espera) return <TelaEspera />
-        
-    if(emExecucao === 'feedback') return <Feedback />
-    
-    return (
-        <Prova  questao={questoesProva} 
-                horaTermino={prova.horaTermino} 
-                history={props.history} 
-                listaRespostas={listaRespostasVazias} 
-                idProva={prova.id}/>
-    );
+    if (emExecucao === 'feedback') return <Feedback />
+
+    if (questoesProva.length > 0)
+        return (
+            <Prova questao={questoesProva}
+                horaTermino={prova[0].horaTermino}
+                history={props.history}
+                listaRespostas={listaRespostasVazias}
+                idProva={prova.id} />
+        );
+
+
+    return <TelaEspera />
+
 }
